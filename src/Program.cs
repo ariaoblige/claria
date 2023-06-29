9bb428a08659d0d05348c76dee041b957f2028a0
ariaoblige <madotserket@gmail.com>
2023-06-29 09:41:45 -0300

    add aliases

--- src/Program.cs
+++ src/Program.cs
@@ -6,30 +6,41 @@ using System.IO;
 namespace arialibs {
   public class MainProg {
     public static void Main(String[] args) {
-      string[] cmds = new string[] {
-        "compile",
-        "create",
-        "aria",
-        "list",
-      };
+      List<string[]> cmds = new List<string[]>() { };
+
+      cmds.Add(new string[] { "compile" });
+      cmds.Add(new string[] { "create", "touch" });
+      cmds.Add(new string[] { "aria" });
+      cmds.Add(new string[] { "list", "ls" });
       
       Console.OutputEncoding = System.Text.Encoding.Unicode;
-      Dictionary<string, Action> ops = new Dictionary<string, Action>();
+      Dictionary<string[], Action> ops = new Dictionary<string[], Action>();
 
       ops.Add(cmds[0], () => Functions.Compile());
       ops.Add(cmds[1], () => Functions.Create());
       ops.Add(cmds[2], () => Functions.AriaCmd());
       ops.Add(cmds[3], () => Functions.List());
 
-      Dictionary<string, string> hlp = new Dictionary<string, string>();
+      Dictionary<string[], string> hlp = new Dictionary<string[], string>();
   
       hlp.Add(cmds[0], "Compiles a file written in C, C++, C# or Java.");
       hlp.Add(cmds[1], "Creates a new file.");
       hlp.Add(cmds[3], "Lists files and directories.");
+      
+      string[] exec     = new string[] { };
 
       if (args.Length > 0) {
+        // Check args
+        foreach(var cmd in cmds) {
+          foreach(var alias in cmd) {
+            if(args[0]==alias) {
+              exec=cmd;
+            }
+          }
+        }
+
         try {
-          ops[args[0]]();
+          ops[exec]();
         }
         catch {
           Visuals.WriteColor("Not a valid command.", ConsoleColor.Red);
@@ -38,7 +49,25 @@ namespace arialibs {
       }
       else {
         foreach(var h in hlp) {
-          Visuals.WriteColor(h.Key, ConsoleColor.Yellow);
+          for(var c=0; c<h.Key.Length; c++) {
+            if (c==0) {
+              Visuals.WriteColor(h.Key[c], ConsoleColor.Yellow);
+            }
+            else if (c==1) {
+              if (c==h.Key.Length-1) {
+                Visuals.WriteColorN("aliases: "+h.Key[c]+"\n", ConsoleColor.DarkGray);
+              }
+              else {
+                Visuals.WriteColorN("aliases: "+h.Key[c], ConsoleColor.DarkGray);
+              }
+            }
+            else if (c==h.Key.Length-1){
+              Visuals.WriteColorN(h.Key[c]+"\n", ConsoleColor.DarkGray);
+            }
+            else {
+              Visuals.WriteColorN(", "+h.Key[c], ConsoleColor.DarkGray);
+            }
+          }
           Visuals.WriteColor("    "+h.Value, ConsoleColor.Blue);
           Console.WriteLine();
         }
--- src/Visuals.cs
+++ src/Visuals.cs
@@ -2,6 +2,14 @@ using System;
 
 namespace arialibs {
   public class Visuals {
+    public static void WriteColorN(string write, ConsoleColor fg=ConsoleColor.Gray, ConsoleColor bg=ConsoleColor.Black) {
+      Console.ForegroundColor = fg;
+      Console.BackgroundColor = bg;
+      Console.Write(write);
+      Console.ResetColor();
+    }
+
+
     public static void WriteColor(string write, ConsoleColor fg=ConsoleColor.Gray, ConsoleColor bg=ConsoleColor.Black) {
       Console.ForegroundColor = fg;
       Console.BackgroundColor = bg;
